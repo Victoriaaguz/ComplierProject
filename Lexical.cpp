2cfcd36995d65953bfda4545fc6cfcc30cbb6474
victoriaaguz <victoriaaguz@csu.fullerton.edu>
2024-03-19 17:25:53 -0700

    fixed keyword list

--- Lexical.cpp
+++ Lexical.cpp
@@ -26,11 +26,11 @@ struct Token
 };
 
 unordered_set<string> keywords = {
-    "False", "None", "True", "and", "as", "assert", "async", "await",
-    "break", "class", "continue", "def", "del", "elif", "else", "except",
+    "False","false", "None", "True","true", "and", "as", "assert", "async", "await",
+    "break", "class", "continue", "def", "del", "elif","endif","endwhile", "else", "except",
     "finally", "for", "from", "global", "if", "import", "in", "is",
     "lambda", "nonlocal", "not", "or", "pass", "raise", "return", "try",
-    "while", "with", "yield"};
+    "while", "with", "yield", "scan"};
 
 bool isSeparator(char ch)
 {
